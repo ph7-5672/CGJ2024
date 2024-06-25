270d5b88f3ee40efb0ea4de2fc34e74b2e93289b
ph7-5672 <51991906+ph7-5672@users.noreply.github.com>
2024-06-25 17:50:15 +0800

    [测试代码]

---
+++ code/Game.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Godot;
+
+namespace Cgj_2024.code;
+
+public partial class Game : Node2D
+{ 
+    
+}
---
+++ code/Game_UI.cs
@@ -0,0 +1,19 @@
+﻿
+using ImGuiNET;
+
+namespace Cgj_2024.code;
+
+public partial class Game
+{
+    public override void _Process(double delta)
+    {
+        ImGui.Begin("test");
+
+        ImGui.Text("你好世界！");
+        ImGui.Text("Hello world!");
+        ImGui.Text("1234567890");
+
+        ImGui.End();
+    }
+}
+
