b924e41961230556188f118ed8496fb4986aad1b
ph7-5672 <51991906+ph7-5672@users.noreply.github.com>
2024-07-04 18:23:50 +0800

    [ImGui文本函数封装]

--- code/Game_UI.cs
+++ code/Game_UI.cs
@@ -1,19 +1,57 @@
 ﻿
+using Godot;
+using ImGuiGodot;
 using ImGuiNET;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Cgj_2024.code;
 
 public partial class Game
 {
+
+
+    void EnterTree_UI()
+    {
+    }
+
+
     public override void _Process(double delta)
     {
         ImGui.Begin("test");
-
-        ImGui.Text("你好世界！");
-        ImGui.Text("Hello world!");
-        ImGui.Text("1234567890");
-
+        Text("测试！", Colors.Cyan);
+        Text(
+            ("青", Colors.Cyan),
+            ("橙", Colors.Orange),
+            ("黄", Colors.Yellow),
+            ("绿", Colors.Green),
+            ("蓝", Colors.Blue),
+            ("靛", Colors.DarkBlue),
+            ("紫", Colors.Purple)
+            );
         ImGui.End();
+        ImGuiGD.Scale = 4;
     }
+
+
+    public static void Text(string text, Color color)
+    {
+        ImGui.PushStyleColor(ImGuiCol.Text, color.ToVector4());
+        ImGui.Text(text);
+        ImGui.PopStyleColor();
+    }
+
+    public static void Text(params (string, Color)[] texts)
+    {
+        for (int i = 0; i < texts.Length; ++i) 
+        {
+            if (i > 0)
+            {
+                ImGui.SameLine();
+            }
+            var text = texts[i];
+            Text(text.Item1, text.Item2);
+        }
+    }
+
 }
 
