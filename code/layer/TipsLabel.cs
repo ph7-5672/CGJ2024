8b66a9af37725bd68f3355afe285f5495c596ad5
YPZhou <yunpeng_zhou@163.com>
2024-07-07 09:12:12 +0800

    AI行动阶段隐藏TipsLabel

--- code/layer/TipsLabel.cs
+++ code/layer/TipsLabel.cs
@@ -1,4 +1,5 @@
 ﻿using Cgj_2024.code;
+using Cgj_2024.code.BackEnd.Factions;
 using Cgj_2024.code.BackEnd.Phase;
 using Godot;
 
@@ -8,25 +9,33 @@ public partial class TipsLabel : Label
     public override void _Process(double delta)
     {
         var phase = Game.Instance.World.CurrentPhase;
-        if (phase is BeginPhase)
+        if (Game.Instance.World.CurrentTurn.CurrentRound.CurrentContorl is Goblin) // Player Round
         {
-            Text = "你的回合";
+            Visible = true;
+            if (phase is BeginPhase)
+            {
+                Text = "你的回合";
+            }
+            if (phase is SelectTerritoryPhase)
+            {
+                Text = "请选择要进攻的人类领地";
+            }
+            if (phase is MobilisePhase)
+            {
+                Text = "请选择要动员的部落";
+            }
+            if (phase is SettlePhase)
+            {
+                Text = "要把领地赏赐给哪个部落？";
+            }
+            if (phase is SelectTerritoryPhase)
+            {
+                Text = "请选择要进攻的人类领地";
+            }
         }
-        if (phase is SelectTerritoryPhase)
+        else // AI Round
         {
-            Text = "请选择要进攻的人类领地";
-        }
-        if (phase is MobilisePhase)
-        {
-            Text = "请选择要动员的部落";
-        }
-        if (phase is SettlePhase)
-        {
-            Text = "要把领地赏赐给哪个部落？";
-        }
-        if (phase is SelectTerritoryPhase)
-        {
-            Text = "请选择要进攻的人类领地";
+            Visible = false;
         }
     }
 
